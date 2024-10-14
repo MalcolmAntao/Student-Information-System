83c6b4c519d5f27e115c78a7b4a8843851933e15
Malcolm Antao <2214025@dbcegoa.ac.in>
2024-10-14 05:47:55 +0530

    Made changes to integrate the code with backend

--- PHP/Connection.php
+++ PHP/Connection.php
@@ -8,7 +8,7 @@
     {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;",$user,$pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        echo"Connection Successful";
+        //echo"Connection Successful";
     }
 
     catch(PDOException $e)
